66cb29e511261ca1e1de53238a5114c5f654626c
Mou <m.mounir.f@gmail.com>
2021-04-15 12:10:48 +0200

    Add hex color utility + use constants to modify app globals

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_boilerplate/utils/hexColor.dart';
+
+const String APP_TITLE = "My Web View App";
+const String WEBSITE_URL = "https://www.YOUR_WEBSITE_URL.com";
+final Color BRAND_COLOR = getColorFromHex('#64dd17');
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_boilerplate/constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,18 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: APP_TITLE,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: BRAND_COLOR,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
