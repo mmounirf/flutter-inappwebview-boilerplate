5f6dc14131ffece3a094e218349a476f2f7a7347
Mou <m.mounir.f@gmail.com>
2021-04-19 13:15:20 +0200

    style status bar with brand color

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_webview_boilerplate/constants.dart';
 import 'package:flutter_webview_boilerplate/widgets/webview-container.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: WebViewContainer(),
-    ));
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(statusBarColor: BRAND_COLOR),
+        child: Scaffold(
+          body: SafeArea(child: WebViewContainer()),
+        ));
   }
 }
